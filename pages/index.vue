3fe0d8c96153c7943a96ed14caa127fc0442fd45
You yongsoo <youyongsoo@fcs.focus-s.com>
2022-07-06 14:12:51 +0900

    デザイン修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,36 @@
 <template>
-  <div>
-    <img :src="response.details.ext_1.url" width="800" />
-    <div>{{ response.details.ext_2 }}</div>
-    <div>{{ response.details.ext_3 }}</div>
+  <main>
+    <header
+      class="hero"
+      :style="{ backgroundImage: `url(${response.details.ext_1.url})` }"
+    >
+      <div class="hero__text">
+        <h1>{{ response.details.ext_2 }}</h1>
+        <p>{{ response.details.ext_3 }}</p>
+      </div>
+    </header>
+    <section>
+      <h2>WORKS</h2>
+      <ul>
+        <li
+          v-for="n in response.details.ext_4"
+          :key="n.slag"
+          class="works__item"
+        >
+          <img :src="n.ext_4.url" />
+          <div class="works__item__text">
+            <h3>{{ n.ext_5 }}</h3>
+            <p>{{ n.ext_6 }}</p>
+          </div>
+        </li>
+      </ul>
+    </section>
 
-    <div v-for="n in response.details.ext_4" :key="n.slag">
-      <img :src="n.ext_4.url" width="400" />
-      <div>{{ n.ext_5 }}</div>
-      <div>{{ n.ext_6 }}</div>
-    </div>
-
-    <!-- response.details.ext_7  -->
-  </div>
+    <section class="about">
+      <h2>ABOUT</h2>
+      <p v-html="response.details.ext_7"></p>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -29,3 +48,104 @@ export default {
   },
 };
 </script>
+
+<style>
+body {
+  margin: 0;
+  font-size: 1em;
+  line-height: 1.5;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+img {
+  max-width: 100%;
+}
+section {
+  max-width: 1200px;
+  margin: 3em auto;
+  padding: 0 20px;
+}
+h1 {
+  margin: 0.5em 0;
+  font-size: 1.8em;
+}
+h2 {
+  margin: 2em auto;
+  font-size: 1.5em;
+  text-align: center;
+}
+h3 {
+  margin: 1em auto;
+  font-size: 1.2em;
+}
+p {
+  margin: 1em 0;
+  font-size: 0.75em;
+}
+.hero {
+  position: relative;
+  width: 100%;
+  height: 300px;
+  background-position: center center;
+  background-size: cover;
+}
+.hero__text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 0 20px;
+}
+.works__item:not(:first-child) {
+  margin-top: 3em;
+}
+@media screen and (max-width: 767px) {
+  .hero::before {
+    width: 100%;
+    height: 300px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+    content: "";
+  }
+  .hero__text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #fff;
+    z-index: 10;
+  }
+}
+@media screen and (min-width: 768px) {
+  body {
+    font-size: 2em;
+  }
+  .hero {
+    height: 600px;
+  }
+  .hero__text {
+    width: 1200px;
+    align-items: flex-end;
+    margin: auto;
+  }
+  .hero__text p {
+    width: 600px;
+  }
+  .works__item {
+    display: flex;
+  }
+  .works__item img {
+    width: 400px;
+    margin-right: 2em;
+  }
+  .about {
+    text-align: center;
+  }
+}
+</style>
