3dd3876f5b6543ba568cc7254b599f36ccb0c2c1
You yongsoo <youyongsoo@fcs.focus-s.com>
2022-07-06 13:18:39 +0900

    検証のため、一度戻す

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,4 @@
 <template>
-  <div>
-    <img :src="response.details.ext_1.url" width="800" />
-    <div>{{ response.details.ext_2 }}</div>
-    <div>{{ response.details.ext_3 }}</div>
-
-    <div v-for="n in response.details.ext_4" :key="n.slag">
-      <img :src="n.ext_4.url" width="400" />
-      <div>{{ n.ext_5 }}</div>
-      <div>{{ n.ext_6 }}</div>
-    </div>
-
-    <!-- response.details.ext_7  -->
-  </div>
+  <Tutorial/>
 </template>
 
-<script>
-export default {
-  async asyncData({ $axios, app }) {
-    try {
-      const response = await $axios.$get(
-        process.env.BASE_URL + "/rcms-api/4/service/8"
-      );
-      console.log(response);
-      return { response };
-    } catch (e) {
-      console.log(e.message);
-    }
-  },
-};
-</script>
